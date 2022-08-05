d98f58a329455021190e8c1b50fda4ddf8507047
Reko <alitahir358@gmail.com>
2022-08-06 00:18:13 +0500

    first merit lists for all programs july 2022

--- prp-merit-list.py
+++ prp-merit-list.py
@@ -27,8 +27,9 @@ import requests
 from json import dump,load
 from docx import Document
 import re
-
-
+from time import sleep
+from os.path import exists
+from os import getcwd, mkdir
 
 TYPE_PROGRAM =  {
         'fcps': 1,
@@ -56,8 +57,7 @@ def get_merit_by_type(program_name: str, page: int):
     '''
    
 
-    url = 'https://prp.phf.gop.pk/MeritGazat/MeritGetAllByTypeView'    
-    print('https://prp.phf.gop.pk/phft/merit/list-'+TYPE_PROGRAM[program_name])
+    url = 'http://prp.phf.gop.pk/MeritGazat/MeritGetAllByTypeView'    
     r = requests.post(url, params={
         'pageNum': int(page),
         'top': 20,
@@ -65,11 +65,12 @@ def get_merit_by_type(program_name: str, page: int):
         'search': ''
     },
     headers= {
-        'Referer': 'https://prp.phf.gop.pk/phft/merit/list-'+TYPE_PROGRAM[program_name],
+        'Referer': 'https://prp.phf.gop.pk/phftt/merit/list-'+program_name,
         'origin': 'https://prp.phf.gop.pk',
-        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36'
-    },
-    verify=False)
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36',
+        'content-type' : 'application/json; charset=utf-8'
+    })
+    print(r.headers)
     return r.json()
 
 
@@ -115,7 +116,7 @@ def save_file(data: list, filename: str):
     doc.save(filename+'.docx')
 
 
-def get_whole_merit(program_name: str , page: int):
+def get_whole_merit(program_name: str):
     '''
     
     Gets a whole merit of program [program_name]
@@ -134,7 +135,9 @@ def get_whole_merit(program_name: str , page: int):
     while len(curr_data) != 0:
         data += curr_data
         page += 1
+        sleep(2)
         curr_data = get_merit_by_type(program_name, page)
+
     return data
 
 
@@ -195,6 +198,40 @@ def get_whole_gazat(program_name: str):
         curr_data = get_gazat_by_type(program_name, page)
     return data
 
+def save_stats(year: str, month:str, merit_list: str):
+    '''
+    save stats.md in the folder merit/year/month/
+    '''
+    Text = f'### {month} {year}\n'
+    Text += '| Program  |  Speciality  | Highest | Lowest |  \n| ------ | ------ | ------ | ------ |\n'
+    if exists('merit_stats.json'):
+        with open('merit_stats.json', 'r') as f:
+            data = load(f)
+        for prog in data:
+            percs = data[prog]
+            for sub in percs:
+                print(percs[sub])
+                Text += f'| {prog.upper()} | {sub} | {max(percs[sub])} | {min(percs[sub])}| \n'
+    check_path(f'merit/{year}/{month}/{merit_list}/')
+    with open(f'merit/{year}/{month}/{merit_list}/stats.md', 'w') as f:
+        f.write(Text)
+
+def check_path(path: str):
+    '''
+    creates the path if not exists
+    '''
+    
+    path_splitted = path.split('/')
+    path_base = getcwd()
+
+    for i in range(len(path_splitted)):
+
+        path_ = path_base +'/'+'/'.join(path_splitted[:i+1])
+        print('checking path if exists', path_)
+        if not exists(path_):
+            print('creating directory', path_.split('/')[-1], 'root directory: ', '[', path_base, ']')
+            mkdir(path_)
+
 ''''
 
 These small lines save whole gazat of all programs
@@ -203,8 +240,25 @@ These small lines save whole gazat of all programs
 '''
 
 
+def get_all_merits():
+    '''
+    saves all merit list to documents and stats in merit_stats.json
+    '''
+    data_perc = {}
 
-programs = ['fcps', 'ms', 'md', 'mds', 'fcpsd']
-for program in programs:
-    data = get_whole_gazat(program)
-    save_file(data, program)
+    programs = ['fcps', 'ms', 'md', 'mds', 'fcpsd']
+    for program in programs:
+        data = get_whole_merit(program)
+        for item in data:
+            if program not in data_perc:
+                data_perc[program] = {}
+            if item['specialityName'] not in data_perc[program]:
+                data_perc[program][item['specialityName']] = []
+            data_perc[program][item['specialityName']].append(item['marks'])
+        save_file(data, program)
+
+    with open('merit_stats.json', 'w') as f:
+        dump(data_perc, f, indent=1)
+
+
+save_stats('2022', 'july', 'first')
